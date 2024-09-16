15b18162adbe57aa7174a44cfbd85852daa545ac
PentestPuppy <170636746+PentestPuppy@users.noreply.github.com>
2024-09-16 15:33:12 -0400

    Create grepfor.sh

---
+++ grepfor.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# GrepFor.sh
+# CLI utility for grepping a target file for a list of words
+
+# By TrashPuppy 2024
+
+# USE: bash ./grepfor.sh <list of terms to grep> <file to grep in>
+# OR : bash ./grepfor.sh, then enter the list name and file name when prompted
+
+echo ''
+echo '            :-- GREP FOR --:'
+echo ''
+
+toGrepList=$1
+target=$2
+
+if [[ -z "$1" ]]; then
+        echo ":-- Enter the to-grep list (should be a file w/ new-line separated strings): "
+        read -a toGrepList
+fi
+
+if [[ -z "$2" ]]; then
+        echo ":-- Enter the file to grep: "
+        read -a target
+fi
+
+list=$(cat $toGrepList)
+echo ""
+echo ':-- Starting...'
+
+for str in $list; do
+        grepWorthit=$(grep -c $str $target)
+
+        if [[ $grepWorthit -eq 0 ]]; then
+                continue
+        else
+                echo -e ":-- Grepping for \033[32m$str\033[0m"
+                echo "$grepWorthit results:"
+                echo ""
+                grep "$str" $target --color
+                echo ""
+        fi
+
+done
+
+echo ':-- Finished'
